91494b747df5d6bb9794bd93cbf09159eb9ce24b
Douglas-Marques <you@example.com>
2021-06-13 05:08:52 -0300

    test: code coverage

--- math_test.go
+++ math_test.go
@@ -9,3 +9,35 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da soma é inválido! Resultado: %d. Valor esperado: %d", total, 30)
 	}
 }
+
+func TestSub(t *testing.T) {
+	total := Sub(30, 15)
+
+	if total != 15 {
+		t.Errorf("Resultado da sub é inválido! Resultado: %d. Valor esperado: %d", total, 15)
+	}
+}
+
+func TestTimes(t *testing.T) {
+	total := Times(5, 5)
+
+	if total != 25 {
+		t.Errorf("Resultado da sub é inválido! Resultado: %d. Valor esperado: %d", total, 25)
+	}
+}
+
+func TestDivides(t *testing.T) {
+	total := Divide(10, 2)
+
+	if total != 5 {
+		t.Errorf("Resultado da sub é inválido! Resultado: %d. Valor esperado: %d", total, 5)
+	}
+}
+
+func TestPower(t *testing.T) {
+	total := Power(2, 2)
+
+	if total != 4 {
+		t.Errorf("Resultado da sub é inválido! Resultado: %d. Valor esperado: %d", total, 4)
+	}
+}
